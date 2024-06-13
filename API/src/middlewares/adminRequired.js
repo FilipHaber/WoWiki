748a6e574ea3d81e80e746cf474c1b902d27a3eb
FilipHbr <hbr.filip@gmail.com>
2024-06-13 15:13:47 +0200

    ajout du middleware adminRequired

--- API/src/middlewares/adminRequired.js
+++ API/src/middlewares/adminRequired.js
@@ -1,11 +1,11 @@
-export default (req, res, next) => {
+function isAdmin(req, res, next) {
   if (!req.session.user || !req.session.user.isAdmin) {
-    res
-      .status(403)
-      .json({ message: "Vous n'êtes pas autorisé à effectuer cette action" });
+    res.status(403).json({ message: "Vous n'avez pas l'autorisation" });
     return;
   }
   if (req.session.user.isAdmin) {
     next();
   }
-};
+}
+
+export default isAdmin;
